7528698e603b676e33cb434b2f36e82919b8c935
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2024-11-17 07:53:36 +0800

    更新 style.css

--- 1.js
+++ 1.js
@@ -1,19 +1,18 @@
 function toggleSwitch(buttonId) {
-    // 获取对应的按钮、标签、音频元素
     const toggle = document.getElementById(`toggle${buttonId}`);
     const label = document.getElementById(`label${buttonId}`);
     const switchElement = document.getElementById(`switch${buttonId}`);
-    const music = document.getElementById(`music${buttonId}`);  // 获取对应的音频元素
+    const music = document.getElementById(`music${buttonId}`);
     const isPlaying = toggle.classList.toggle('on');
 
-    // 控制音频的播放与暂停
+    // 根据按钮状态切换图标和播放/暂停音频
     if (isPlaying) {
         music.play();
-        label.textContent = '播放';  // 更新按钮的文本
+        label.textContent = '播放';  // 更新按钮文本
         switchElement.setAttribute('aria-checked', 'true');  // 更新无障碍属性
     } else {
         music.pause();
-        label.textContent = '暂停';  // 更新按钮的文本
+        label.textContent = '暂停';  // 更新按钮文本
         switchElement.setAttribute('aria-checked', 'false');  // 更新无障碍属性
     }
 }
